861b8da7051aa867dd737c238f65d0fea0a61039
ZhannaAnisimova <zhzhv@yandex.ru>
2025-03-10 21:23:11 +0300

    Update Main.java

--- src/main/java/ru/zhzhv/Main.java
+++ src/main/java/ru/zhzhv/Main.java
@@ -1,10 +1,10 @@
 package ru.zhzhv;
 
 public class Main {
-
-private static string msg = ""Hello, qa guru la la la!!!!!!!!!!""
-    
-    public static void main(String[] args) {
-        System.out.println(msg);
-    }
+  
+  private static String msg = "Hello qa.guru!";
+  
+  public static void main(String... args) {
+    System.out.println(msg);
+  }
 }
