09d3ad85929ad582cf511d67691dad92aab09bfe
ZhannaAnisimova <zhzhv@yandex.ru>
2025-03-10 21:47:54 +0300

    Update Main.java

--- src/main/java/ru/zhzhv/Main.java
+++ src/main/java/ru/zhzhv/Main.java
@@ -2,7 +2,7 @@ package ru.zhzhv;
 
 public class Main {
   
-  private static String msg = "Hello world!!";
+  private static String msg = "Hello Misha!";
   
   public static void main(String... args) {
     System.out.println(msg);
