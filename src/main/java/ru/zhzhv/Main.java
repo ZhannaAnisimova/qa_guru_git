25334bfbdc20ba3e54f949e545ac654cbc698f16
Zhanna Anisimova <zhzhv@yandex.ru>
2025-03-10 21:45:38 +0300

    added qa guru

--- src/main/java/ru/zhzhv/Main.java
+++ src/main/java/ru/zhzhv/Main.java
@@ -2,7 +2,7 @@ package ru.zhzhv;
 
 public class Main {
   
-  private static String msg = "Hello world!!";
+  private static String msg = "Hello qa guru!!!!!!!";
   
   public static void main(String... args) {
     System.out.println(msg);
